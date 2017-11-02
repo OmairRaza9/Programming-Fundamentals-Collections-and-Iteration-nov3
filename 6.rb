f77e8d7dc9cdebb47fc2319e81626bb236fff75e
Omair <omairraza9@gmail.com>
2017-11-02 17:35:15 -0400

    finished assignment 6

--- 6.rb
+++ 6.rb
@@ -11,20 +11,36 @@ movies = {looper: 2011, scott_pilgrem: 2008, forrest_gump: 1998}
 cities= {toronto: 2, houston: 1.5, karachi: 3}
 family = {Zohaib: 29, Atef: 27, Saarim: 38, Waq: 19}
 #1
+#movies = []
 film = {}
 film[1999] = ["The Matrix", "Star Wars Episode 1", "The Mummy"]
-film[2009] = [ "avatar", "star_trek" "district_9"]
-film[2019] = ["how_to_train_a_dragon" "toy_story" "star_wars_9"]
-p film
+film[2009] = [ "avatar", "star_trek", "district_9"]
+film[2019] = ["how_to_train_a_dragon", "toy_story", "star_wars_9"]
+
+#movies << film
+p movies
 #2
 phone = []
 row_1 = ["1 2 3"]
 row_2 = ["4 5 6"]
 row_3 = ["7 8 9"]
 row_4 = ["* 0 #"]
-
  phone << row_1
 phone << row_2
 phone << row_3
 phone << row_4
 p phone
+#3hash that has a name, a continent, and whether or not it is an island.
+puts "+++++++++++++++++++++++++++++++++++++++++++"
+globe = [{:continent => "North America",:name => "Canada",   :island => "not_island"},
+         {:continent => "North America",:name => "Cuba",     :island => "island"},
+         {:continent => "Asia"         ,:name => "Pakistan", :island => "not_island"}]
+# country = {}
+#
+# country[canada]   = ["north_america", "not_island"]
+# country[cuba]     = ["north_america", "island"]
+# country[pakistan] = ["asia", "not_island"]
+#
+# globe << country
+
+p globe
